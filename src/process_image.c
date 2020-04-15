7d7b4e74b6959725f9cca846de6c0e924c2f285d
Seyoung Park <seyoung.arts.park@protonmail.com>
2020-04-15 22:32:08 +0300

    Do extras

--- src/process_image.c
+++ src/process_image.c
@@ -49,6 +49,14 @@ image copy_image(image im)
     return copy;
 }
 
+void scale_image(image im, int c, float v) {
+    // Input image must be in HSL space if you want to use it for saturation control like in the exercise
+    const int image_area = im.w * im.h;
+    for(int i = 0; i < image_area; i++) {
+        im.data[image_area * c + i] *= v;
+    }
+}
+
 image rgb_to_grayscale(image im)
 {
     assert(im.c == 3);
@@ -166,4 +174,120 @@ void hsv_to_rgb(image im)
     }
 
     // TODO Fill this in
+}
+
+void rgb_to_ciexyz(image im) {
+    // srgb to CIEXYZ
+    const int size = im.h * im.w;
+    float r = 0, g = 0, b = 0;
+    
+    for(int i = 0; i < size; i++){
+        r = im.data[i];
+        g = im.data[i + size];
+        b = im.data[i + size * 2];
+
+        im.data[i] = (0.412453 * r + 0.35758 * g + 0.180423 * b);
+        im.data[i + size] = (0.212671 * r + 0.71516 * g + 0.072169 * b);
+        im.data[i + size * 2] = (0.019334 * r + 0.119193 * g + 0.950227 * b);
+    }
+
+}
+
+void rgb_to_ciexyy(image im) {
+    // CIExyY
+    const int size = im.h * im.w;
+    float r = 0, g = 0, b = 0;
+    float X = 0, Y = 0, Z = 0;
+    
+    for(int i = 0; i < size; i++){
+        r = im.data[i];
+        g = im.data[i + size];
+        b = im.data[i + size * 2];
+
+        X = (0.412453 * r + 0.35758 * g + 0.180423 * b);
+        Y = (0.212671 * r + 0.71516 * g + 0.072169 * b);
+        Z = (0.019334 * r + 0.119193 * g + 0.950227 * b);
+
+        im.data[i] = X / (X + Y + Z);
+        im.data[i + size] = Y / (X + Y + Z);
+        im.data[i + size * 2] = Y;
+    }
+
+}
+
+void rgb_to_cieluv(image im) {
+    // srgb to cieluv
+    // https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html
+    const int size = im.h * im.w;
+    float r = 0, g = 0, b = 0;
+    float X = 0, Y = 0, Z = 0;
+    float un = 0.19793;
+    float vn = 0.46831;
+    float L = 0;
+    float u_prime = 0, v_prime = 0;
+    float u = 0, v = 0;
+
+    for(int i = 0; i < size; i++){
+        r = im.data[i];
+        g = im.data[i + size];
+        b = im.data[i + size * 2];
+
+        X = (0.412453 * r + 0.35758 * g + 0.180423 * b);
+        Y = (0.212671 * r + 0.71516 * g + 0.072169 * b);
+        Z = (0.019334 * r + 0.119193 * g + 0.950227 * b);
+
+        if (Y > 0.008856) {
+            L = 116 * pow(Y, 1/3) - 16;
+        } else {
+            L = 903.3 * Y;
+        }
+
+        u_prime = 4 * X / (X + 15 * Y + 3 * Z);
+        v_prime = 9 * Y / (X + 15 * Y + 3 * Z);
+        
+        u = 13 * L * (u_prime - un);
+        v = 13 * L * (v_prime - vn);
+
+        im.data[i] = L;
+        im.data[i + size] = u;
+        im.data[i + size * 2] = v;
+    }
+
+}
+
+void ciexyy_to_cieyuv(image im) {
+    const int size = im.h * im.w;
+    float x = 0, y = 0, y_luminance = 0;
+    float u = 0, v = 0;
+    float denominator = 0;
+    
+    for(int i = 0; i < size; i++){
+        x = im.data[i];
+        y = im.data[i + size];
+        y_luminance = im.data[i + size * 2];
+
+        denominator = (6*y - x + 1.5);
+        im.data[i] = y_luminance;
+        im.data[i + size] = 2 * x / denominator;
+        im.data[i + size * 2] = 4.5 * y / denominator;
+    }
+}
+
+void ciexyz_to_cieluv(image im) {
+    // https://stackoverflow.com/a/49414800/3067013
+    // https://poynton.ca/PDFs/coloureq.pdf
+    // https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html
+
+    // XYZ to xyY
+
+    // u' and v'
+    float yn = 5.650675;
+    float un = 0.210526;
+    float vn = 0.473684;
+    // L*, u*, v*
+
+}
+
+void cieluv_to_hcl(image im) {
+
 }
